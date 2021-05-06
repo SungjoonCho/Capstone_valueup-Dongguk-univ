fd89ce423b2e33d40bdbc705bd9aebb2802142cc
SungjoonCho <80872528+SungjoonCho@users.noreply.github.com>
2021-05-06 14:58:51 +0900

    Update Naver_TTS.py

--- Naver_TTS.py
+++ Naver_TTS.py
@@ -3,8 +3,10 @@
 import os
 import sys
 import urllib.request
+
 client_id = ""
 client_secret = ""
+
 encText = urllib.parse.quote("안녕하세요")
 data = "speaker=mijin&speed=0&text=" + encText;
 url = "https://naveropenapi.apigw.ntruss.com/voice/v1/tts"
@@ -13,6 +15,7 @@ request.add_header("X-NCP-APIGW-API-KEY-ID",client_id)
 request.add_header("X-NCP-APIGW-API-KEY",client_secret)
 response = urllib.request.urlopen(request, data=data.encode('utf-8'))
 rescode = response.getcode()
+
 if(rescode==200):
     print("TTS mp3 저장")
     response_body = response.read()
